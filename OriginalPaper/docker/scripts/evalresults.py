9134bdf34a92f38cbf6ceb1dce407f45c38ba6ae
JonasElburgUVA <114097649+JonasElburgUVA@users.noreply.github.com>
2023-05-10 08:52:14 +0200

    Error message and anonymous paths

--- OriginalPaper/docker/scripts/evalresults.py
+++ OriginalPaper/docker/scripts/evalresults.py
@@ -7,7 +7,7 @@ from zipfile import ZipFile
 import numpy as np
 import scipy.spatial.distance as distance
 
-def getDSC(label, vol, t=0.35):
+def get_dsc(label, vol, t=0.35):
     # threshold t is a hyperparameter
     TP = sum(vol[label==1] >= t)
     FP = sum(vol[label==0] >= t)
@@ -20,12 +20,12 @@ def getDSC(label, vol, t=0.35):
 
 def eval_dir(output_dir, label_dir, mode):
     res = None
-    AUROC = []
-    AP = []
+    auroc = []
+    ap = []
     if mode == "pixel":
         output_dir = os.path.join(output_dir, "pixel")
         label_dir = os.path.join(label_dir, "pixel")
-        DSC = []
+        dsc = []
 
         for f in os.listdir(output_dir):
             vol = nib.load(os.path.join(output_dir, f))
@@ -34,26 +34,27 @@ def eval_dir(output_dir, label_dir, mode):
             label = np.asarray(label.get_fdata()).flatten()
             assert vol.shape == label.shape
             try:
-                AUROC.append(metrics.roc_auc_score(label,vol))
-                AP.append(metrics.average_precision_score(label, vol))
-                DSC.append(getDSC(label, vol))
+                auroc.append(metrics.roc_auc_score(label,vol))
+                ap.append(metrics.average_precision_score(label, vol))
+                dsc.append(get_dsc(label, vol))
             except:
+                print(f"Could not calculate metrics for {f}. Does the label contain only 0s or 1s? Skipping...")
                 pass
-        res = (np.mean(AUROC), np.mean(AP), np.mean(DSC))
+        res = (np.mean(auroc), np.mean(ap), np.mean(dsc))
         
     elif mode == "sample":
         output_dir = os.path.join(output_dir, "sample")
         label_dir = os.path.join(label_dir, "sample")
-        AUROC = []
-        AP = []
+        auroc = []
+        ap = []
         for f in os.listdir(output_dir):
             with open(os.path.join(output_dir, f), "r") as file:
                 anomaly_score = file.read()
             with open(os.path.join(label_dir, f), "r") as file:
                 label = file.read()
-            AUROC.append(metrics.roc_auc_score(anomaly_score, label))
-            AP.append(metrics.average_precision_score(anomaly_score, label))
-        res = (torch.mean(AUROC), torch.mean(AP))
+            auroc.append(metrics.roc_auc_score(anomaly_score, label))
+            ap.append(metrics.average_precision_score(anomaly_score, label))
+        res = (torch.mean(auroc), torch.mean(ap))
     else:
         raise ValueError("Invalid mode. Must be either 'pixel' or 'sample'.")
 
@@ -67,15 +68,15 @@ if __name__ == "__main__":
         "--output_dir",
         type=str,
         help="Directory containing the predictions.",
-        required=False,
-        default="/Users/jonase/Documents/UVA/DEL2/AnomalyDetection/AnomalyDetection/OriginalPaper/output/full"
+        required=True,
+        default="../../output/full"
     )
     parser.add_argument(
         "--label_dir",
         type=str,
         help="Directory containing the ground truth labels.",
-        required=False,
-        default="/Users/jonase/Documents/UVA/DEL2/AnomalyDetection/AnomalyDetection/OriginalPaper/toy_label"
+        required=True,
+        default="../../toy_label"
     )
     parser.add_argument(
         "--mode",
